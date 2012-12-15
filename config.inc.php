60903e226b6d7bd068db41e7b9303a51777833a4
Azerdar Kusumoto <kusumoto.com@gmail.com>
2012-12-15 17:01:45 +0700

    Update config.inc.php
    Delete data

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 //config.inc.php
 $host = "localhost";
-$user = "joke_kusumoto";
-$pass = "kusumoto$#@!";
-$dbname = "joke_kfriend";
+$user = "";
+$pass = "";
+$dbname = "";
 ?>
